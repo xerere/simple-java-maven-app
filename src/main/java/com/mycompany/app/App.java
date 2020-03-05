75f6a2cc74da2522207f27143f782213890d90a6
unknown <xerere@gmail.com>
2020-03-05 16:04:32 +0000

    some comment to test git triggers3

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,7 +11,7 @@ public class App
     public App() {}
 
     public static void main(String[] args) {
-        //some comment to test git triggers2
+        //some comment to test git triggers3
         System.out.println(new App().getMessage());
     }
 
